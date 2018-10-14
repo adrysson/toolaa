d0e4dc17d1156872c1ec281655522c79bb7223cc
Adrysson <adrysson_santos@msn.com>
2018-10-14 04:35:32 -0300

    MODEL: Testes

---
+++ src/Model/Table/TestesTable.php
@@ -0,0 +1,91 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Testes Model
+ *
+ * @property \App\Model\Table\ArtigosTable|\Cake\ORM\Association\BelongsTo $Artigos
+ * @property \App\Model\Table\FerramentasTable|\Cake\ORM\Association\BelongsTo $Ferramentas
+ * @property \App\Model\Table\UsuariosTable|\Cake\ORM\Association\BelongsTo $Usuarios
+ *
+ * @method \App\Model\Entity\Testis get($primaryKey, $options = [])
+ * @method \App\Model\Entity\Testis newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\Testis[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\Testis|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Testis|bool saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Testis patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\Testis[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\Testis findOrCreate($search, callable $callback = null, $options = [])
+ *
+ * @mixin \Cake\ORM\Behavior\TimestampBehavior
+ */
+class TestesTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->setTable('testes');
+        $this->setEntityClass('App\Model\Entity\Teste');
+        $this->setDisplayField('id');
+        $this->setPrimaryKey('id');
+
+        $this->addBehavior('Timestamp');
+
+        $this->belongsTo('Artigos', [
+            'foreignKey' => 'artigo_id',
+            'joinType' => 'INNER'
+        ]);
+        $this->belongsTo('Ferramentas', [
+            'foreignKey' => 'ferramenta_id',
+            'joinType' => 'INNER'
+        ]);
+        $this->belongsTo('Usuarios', [
+            'foreignKey' => 'usuario_id',
+            'joinType' => 'INNER'
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('id')
+            ->allowEmpty('id', 'create');
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules)
+    {
+        $rules->add($rules->existsIn(['artigo_id'], 'Artigos'));
+        $rules->add($rules->existsIn(['ferramenta_id'], 'Ferramentas'));
+        $rules->add($rules->existsIn(['usuario_id'], 'Usuarios'));
+
+        return $rules;
+    }
+}
---
+++ tests/Fixture/TestesFixture.php
@@ -0,0 +1,63 @@
+<?php
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * TestesFixture
+ *
+ */
+class TestesFixture extends TestFixture
+{
+
+    /**
+     * Fields
+     *
+     * @var array
+     */
+    // @codingStandardsIgnoreStart
+    public $fields = [
+        'id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'autoIncrement' => true, 'precision' => null],
+        'artigo_id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        'ferramenta_id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        'usuario_id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        'created' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => 'CURRENT_TIMESTAMP', 'comment' => '', 'precision' => null],
+        'modified' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => 'CURRENT_TIMESTAMP', 'comment' => '', 'precision' => null],
+        '_indexes' => [
+            'fk_testes_artigos' => ['type' => 'index', 'columns' => ['artigo_id'], 'length' => []],
+            'fk_testes_ferramentas' => ['type' => 'index', 'columns' => ['ferramenta_id'], 'length' => []],
+            'fk_testes_usuarios' => ['type' => 'index', 'columns' => ['usuario_id'], 'length' => []],
+        ],
+        '_constraints' => [
+            'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
+            'fk_testes_artigos' => ['type' => 'foreign', 'columns' => ['artigo_id'], 'references' => ['artigos', 'id'], 'update' => 'restrict', 'delete' => 'cascade', 'length' => []],
+            'fk_testes_ferramentas' => ['type' => 'foreign', 'columns' => ['ferramenta_id'], 'references' => ['ferramentas', 'id'], 'update' => 'restrict', 'delete' => 'cascade', 'length' => []],
+            'fk_testes_usuarios' => ['type' => 'foreign', 'columns' => ['usuario_id'], 'references' => ['usuarios', 'id'], 'update' => 'restrict', 'delete' => 'cascade', 'length' => []],
+        ],
+        '_options' => [
+            'engine' => 'InnoDB',
+            'collation' => 'latin1_swedish_ci'
+        ],
+    ];
+    // @codingStandardsIgnoreEnd
+
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init()
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'artigo_id' => 1,
+                'ferramenta_id' => 1,
+                'usuario_id' => 1,
+                'created' => '2018-10-14 04:32:08',
+                'modified' => '2018-10-14 04:32:08'
+            ],
+        ];
+        parent::init();
+    }
+}
