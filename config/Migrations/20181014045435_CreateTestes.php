5cdcc06bce90c4bd960b088cf9727fd0d30f579e
Adrysson <adrysson_santos@msn.com>
2018-10-14 01:56:20 -0300

    MIGRATIONS: Testes

---
+++ config/Migrations/20181014045435_CreateTestes.php
@@ -0,0 +1,40 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreateTestes extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('testes');
+        $table->addColumn('tipo_id', 'integer', [
+            'default' => null,
+            'limit' => 11,
+            'null' => false,
+        ]);
+        $table->addColumn('bloco_id', 'integer', [
+            'default' => null,
+            'limit' => 11,
+            'null' => false,
+        ]);
+        $table->addColumn('conteudo', 'text', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addForeignKey('tipo_id', 'testes_tipos', 'id', [
+            'delete'=> 'CASCADE',
+            'constraint' => 'fk_testes_tipos'
+        ]);
+        $table->addForeignKey('bloco_id', 'testes_blocos', 'id', [
+            'delete'=> 'CASCADE',
+            'constraint' => 'fk_testes_blocos'
+        ]);
+        $table->create();
+    }
+}
