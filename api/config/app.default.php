d5e3a6e1d37c2a241d98542b22409620559a8179
Adrysson <adrysson_santos@msn.com>
2018-11-29 23:31:13 -0300

    Iniciando autenticação na API

--- api/config/app.default.php
+++ api/config/app.default.php
@@ -255,9 +255,9 @@ return [
              * the following line and set the port accordingly
              */
             //'port' => 'non_standard_port_number',
-            'username' => 'my_app',
-            'password' => 'secret',
-            'database' => 'my_app',
+            'username' => env('DB_USERNAME', 'my_app'),
+            'password' => env('DB_PASSWORD', 'secret'),
+            'database' => env('DB_NAME', 'my_app'),
             /*
              * You do not need to set this flag to use full utf-8 encoding (internal default since CakePHP 3.6).
              */
